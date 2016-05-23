2899602111fd8ac71ea3c4613561c74839028435
tanzhiqiang <tanzhiqiang@tanzhiqiangdeimac.local>
2016-05-23 15:18:00 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 谭志强 on 16/5/23.
+//  Copyright © 2016年 谭志强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 谭志强 on 16/5/23.
+//  Copyright © 2016年 谭志强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
